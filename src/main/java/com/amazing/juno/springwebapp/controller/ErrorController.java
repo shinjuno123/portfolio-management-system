e9072c1474518fc27bc72c07f1bdd7a4c3c1e25c
shinjuno123 <shinjuno123@naver.com>
2023-01-27 23:20:00 +0900

    feat: Pass error message from server to thymeleaf form

--- src/main/java/com/amazing/juno/springwebapp/controller/ErrorController.java
+++ src/main/java/com/amazing/juno/springwebapp/controller/ErrorController.java
@@ -1,18 +1,27 @@
 package com.amazing.juno.springwebapp.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
-	@RequestMapping("/error")
-	public String handleError(HttpServletRequest request) {
+	@GetMapping("/error")
+	public String handleError(Model model, HttpServletRequest request) {
 		Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
 		
-		System.out.println(status);
+		if(status != null) {
+			Integer statusCode = Integer.valueOf(status.toString());
+			if(statusCode == 404) {
+				model.addAttribute("title", "404 Error");
+				model.addAttribute("message", "Page Not Found");
+			}
+		
+		}
+		
 		return "error";
 	}
 }
