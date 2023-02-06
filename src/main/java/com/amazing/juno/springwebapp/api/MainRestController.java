4e981d80a629d03785143a8172c7689e11a654c9
shinjuno123 <shinjuno123@naver.com>
2023-02-07 02:08:53 +0900

    chore: Remove test endpoint

--- src/main/java/com/amazing/juno/springwebapp/api/MainRestController.java
+++ src/main/java/com/amazing/juno/springwebapp/api/MainRestController.java
@@ -2,7 +2,6 @@ package com.amazing.juno.springwebapp.api;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,12 +12,6 @@ import com.amazing.juno.springwebapp.dto.TechnologyListDto;
 @RequestMapping("/api")
 @RestController
 public class MainRestController {
-	
-	@GetMapping("/technology-stacks")
-	public String test(){
-		
-		return "test";
-	}
 	
 	@PostMapping("/technology-stacks")
 	public List<TechnologyListDto> saveStack(@RequestBody List<TechnologyListDto> stacks){
