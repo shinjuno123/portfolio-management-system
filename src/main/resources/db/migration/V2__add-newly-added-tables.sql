f61c2f2361d9c0f519d9aa516074134e3f939de2
Junho Shin <shinjuno123@naver.com>
2023-06-17 14:17:52 -0700

    Chore : Add flyway script for newly added entities

---
+++ src/main/resources/db/migration/V2__add-newly-added-tables.sql
@@ -0,0 +1,28 @@
+CREATE TABLE notification (
+    id CHAR(36) NOT NULL,
+    subject VARCHAR(255) NOT NULL,
+    body TEXT NOT NULL,
+    image_url TEXT NOT NULL,
+    video_url TEXT,
+    active BOOLEAN NOT NULL,
+    displayed BOOLEAN NOT NULL,
+    version INTEGER,
+    updated TIMESTAMP,
+    uploaded TIMESTAMP,
+    PRIMARY KEY (id)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci;
+
+
+CREATE TABLE relevant_site (
+    id CHAR(36) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    url TEXT NOT NULL,
+    version INTEGER,
+    uploaded TIMESTAMP,
+    updated TIMESTAMP,
+    PRIMARY KEY (id)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci;
